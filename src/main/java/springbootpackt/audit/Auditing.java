398c2071422228eadb6e30aa1cd234e98c36a426
Fotis Tsavalos <v-ftsavalos@eurobank.gr>
2018-11-14 10:22:00 +0200

    used @Around to track the duration of execution of a controller method.

--- src/main/java/springbootpackt/audit/Auditing.java
+++ src/main/java/springbootpackt/audit/Auditing.java
@@ -18,7 +18,16 @@ public class Auditing {
     @Around("execution(* springbootpackt.controller.ChapterController.getChapters())")
     public Object logAroundGetChapters(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("Aspect @Around triggered for getChapters endpoint [{}]", joinPoint);
-        return joinPoint.proceed();
+
+        long begin = System.currentTimeMillis();
+
+        // the code above is running before the jointpoint [getChapters() method]
+        Object result  = joinPoint.proceed(); // procceed method is invoking the the execution of the jointpoint
+        //the code below is running after the joinpoint
+
+        long end = System.currentTimeMillis();
+        log.info("Duration of execution of getChapters() is: {}", end - begin);
+        return result;
     }
 
 }
